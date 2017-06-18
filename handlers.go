568d59d4aa0887d5e9f76e2cc51dd83d6f526265
MikeKoval <mykhailo.koval.2015@eliftech.com>
2017-06-18 15:47:56 +0300

    added websockets

--- handlers.go
+++ handlers.go
@@ -1,127 +1,122 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/gorilla/mux"
 	"go-ev3/serialapi"
-	"net/http"
-	"strconv"
+	"github.com/gorilla/websocket"
+	"fmt"
 )
 
 // Response helper functionality
-func writeResponse(w http.ResponseWriter, v interface{}, err error) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+func writeResponse(w *websocket.Conn, v interface{}, err error) {
+	//w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	if err == nil {
-		w.WriteHeader(http.StatusOK)
-		if v != nil {
-			json.NewEncoder(w).Encode(v)
+		if err = w.WriteJSON(v); err != nil {
+			fmt.Println(err)
 		}
 	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(err)
+		w.WriteJSON(err)
 	}
 }
 
 // Get port status
-func GetPortsStatus(w http.ResponseWriter, r *http.Request) {
+func GetPortsStatus(w *websocket.Conn, r WSMsg) {
 	result, err := ev3.GetPortsStatus()
 	writeResponse(w, result, err)
 }
 
 // Plays sound on brick side
-func PlaySound(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	volume, _ := strconv.Atoi(vars["volume"])
-	frequency, _ := strconv.Atoi(vars["frequency"])
-	duration, _ := strconv.Atoi(vars["duration"])
+func PlaySound(w *websocket.Conn, r WSMsg) {
+	volume := r.Volume
+	frequency := r.Frequency
+	duration := r.Duration
 
 	err := ev3.PlaySound(uint8(volume), uint16(frequency), uint16(duration))
 	writeResponse(w, nil, err)
 }
 
 // Get color value for connected sensor
-func GetColor(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func GetColor(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	result, err := ev3.GetColor(uint8(port))
 	resultStr := serialapi.ColorStr(result)
 	writeResponse(w, resultStr, err)
 }
 
 // Get luminosity value for connected sensor
-func GetLuminosity(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func GetLuminosity(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	result, err := ev3.GetLuminosity(uint8(port))
 	writeResponse(w, result, err)
 }
 
 // Get distance value for connected sensor
-func GetDistance(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func GetDistance(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	result, err := ev3.GetDistance(uint8(port))
 	writeResponse(w, result, err)
 }
 
 // Get is clickable sensor clicked
-func GetIsClicked(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func GetIsClicked(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	result, err := ev3.GetIsClicked(uint8(port))
 	writeResponse(w, result, err)
 }
 
 // Get click count for connected sensor
-func GetClickCount(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func GetClickCount(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	result, err := ev3.GetClickCount(uint8(port))
 	writeResponse(w, result, err)
 }
 
 // Get gyro angle for connected sensor
-func GetGyroAngle(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func GetGyroAngle(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	result, err := ev3.GetGyroAngle(uint8(port))
 	writeResponse(w, result, err)
 }
 
 // Get gyro angle for connected sensor
-func GetGyroGravity(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func GetGyroGravity(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	result, err := ev3.GetGyroGravity(uint8(port))
 	writeResponse(w, result, err)
 }
 
 // Get value for generic sensor
-func GetSensorValue(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func GetSensorValue(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	result, err := ev3.GetSensorValue(uint8(port), 0xFF)
 	writeResponse(w, result, err)
 }
 
 // Start motor
-func MotorStart(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func MotorStart(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	err := ev3.MoveMotorStart(uint8(port))
 	writeResponse(w, nil, err)
 }
 
 // Stop motor and (not) apply brake
-func MotorStop(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
-	brake, _ := strconv.Atoi(mux.Vars(r)["brake"])
+func MotorStop(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
+	brake := r.Brake
 	err := ev3.MoveMotorStop(uint8(port), uint8(brake))
 	writeResponse(w, nil, err)
 }
 
 // Set motor speed
-func SetMotorSpeed(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
-	speed, _ := strconv.Atoi(mux.Vars(r)["speed"])
+func SetMotorSpeed(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
+	speed := r.Speed
 	err := ev3.MoveMotorSpeed(uint8(port), int8(speed))
 	writeResponse(w, nil, err)
 }
 
 // Get motor current angle
-func GetMotorState(w http.ResponseWriter, r *http.Request) {
-	port, _ := strconv.Atoi(mux.Vars(r)["port-id"])
+func GetMotorState(w *websocket.Conn, r WSMsg) {
+	port := r.PortId
 	result, err := ev3.GetMotorAngle(uint8(port))
 	writeResponse(w, result, err)
 }
--- router.go
+++ router.go
@@ -1,28 +1,71 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"fmt"
+	"github.com/gorilla/websocket"
+)
 
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/ports", GetPortsStatus).Methods("GET")
-
-	router.HandleFunc("/sound/{volume:[0-9]{1,3}}/frequency/{frequency:[0-9]{1,5}}/duration/{duration:[0-9]{1,5}}",
-		PlaySound).Methods("POST")
-
-	router.HandleFunc("/motors/{port-id:[0-9]{1,3}}/start", MotorStart).Methods("POST")
-	router.HandleFunc("/motors/{port-id:[0-9]{1,3}}/stop/{brake:[0-1]}", MotorStop).Methods("POST")
-	router.HandleFunc("/motors/{port-id:[0-9]{1,3}}/speed/{speed}", SetMotorSpeed).Methods("POST")
-	router.HandleFunc("/motors/{port-id:[0-9]{1,3}}", GetMotorState).Methods("GET")
-
-	router.HandleFunc("/sensors/{port-id:[0-3]}/color", GetColor).Methods("GET")
-	router.HandleFunc("/sensors/{port-id:[0-3]}/luminosity", GetLuminosity).Methods("GET")
-	router.HandleFunc("/sensors/{port-id:[0-3]}/distance", GetDistance).Methods("GET")
-	router.HandleFunc("/sensors/{port-id:[0-3]}/click", GetIsClicked).Methods("GET")
-	router.HandleFunc("/sensors/{port-id:[0-3]}/clickcount", GetClickCount).Methods("GET")
-	router.HandleFunc("/sensors/{port-id:[0-3]}/gyro/angle", GetGyroAngle).Methods("GET")
-	router.HandleFunc("/sensors/{port-id:[0-3]}/gyro/gravity", GetGyroGravity).Methods("GET")
-	router.HandleFunc("/sensors/{port-id:[0-3]}", GetSensorValue).Methods("GET")
+	router.HandleFunc("/ws", wsHandler)
 
 	return router
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	//if r.Header.Get("Origin") != "http://"+r.Host {
+	//	http.Error(w, "Origin not allowed", 403)
+	//	return
+	//}
+	conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+	if err != nil {
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+	}
+
+	go processWsMsg(conn)
+}
+
+type WSMsg struct {
+	Action string
+
+	Volume uint8
+	Frequency uint16
+	Duration uint16
+
+	PortId uint8
+	Speed int8
+	Brake uint8
+}
+
+func processWsMsg(conn *websocket.Conn) {
+	for {
+		m := WSMsg{}
+
+		err := conn.ReadJSON(&m)
+		if err != nil {
+			fmt.Println("Error reading json.", err)
+		}
+
+		fmt.Printf("Got message: %#v\n", m)
+
+		switch m.Action {
+			case "GetPortsStatus": 	GetPortsStatus(conn, m)
+			case "PlaySound": 	PlaySound(conn, m)
+			case "MotorStart": 	MotorStart(conn, m)
+			case "MotorStop": 	MotorStop(conn, m)
+			case "SetMotorSpeed": 	SetMotorSpeed(conn, m)
+			case "GetMotorState": 	GetMotorState(conn, m)
+			case "GetColor": 	GetColor(conn, m)
+			case "GetLuminosity": 	GetLuminosity(conn, m)
+			case "GetDistance": 	GetDistance(conn, m)
+			case "GetIsClicked": 	GetIsClicked(conn, m)
+			case "GetClickCount": 	GetClickCount(conn, m)
+			case "GetGyroAngle": 	GetGyroAngle(conn, m)
+			case "GetGyroGravity": 	GetGyroGravity(conn, m)
+			case "GetSensorValue": 	GetSensorValue(conn, m)
+		}
+	}
 }
